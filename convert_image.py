b81b1d5b6079c97f93651c0b694c35cf026785d9
Zachary Susskind <ZSusskind@gmail.com>
2023-09-18 17:11:27 -0500

    Saturation and tint support

--- convert_image.py
+++ convert_image.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python3
 
 import argparse
+import colorsys
 import numpy as np
-from matplotlib import pyplot as plt
 import skimage as ski
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.ensemble import IsolationForest
+from numba import njit, prange
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Automatically convert image to pixel art")
     parser.add_argument("input_filename", help="File to convert")
     parser.add_argument("output_filename", help="File to write result to")
-    parser.add_argument("--block_size", type=int, default=4, help="Size (nxn) of each pixel in output image")
-    parser.add_argument("--palette_size", type=int, default=8, help="Colors in the output image")
-    parser.add_argument("--accent_palette_size", type=int, default=0, help="Extra colors to make contrast regions \"pop\"")
-    parser.add_argument("--specific_colors", nargs="+", help="Specify extra colors in hex format")
-    parser.add_argument("--neighbor_threshold", type=int, default=8, help="Denoising test; the default value seems good")
+    parser.add_argument("--block_size", type=int, default=4,
+                        help="Edge size of output image pixel in original image pixels")
+    parser.add_argument("--palette_size", type=int, default=8,
+                        help="Total unique colors to use in the output image")
+    parser.add_argument("--accent_size", type=int, default=0,
+                        help="Reserves palette colors for pixels which can't be represented well "\
+                        "by the first-pass palette generation; good for capturing fine details")
+    parser.add_argument("--specific_colors", nargs="+",
+                        help="Specify palette colors in hex format")
+    parser.add_argument("--specific_accents", nargs="+",
+                        help="Ditto, but for accents")
+    parser.add_argument("--denoise", action="store_true",
+                        help="Attempts to smooth spurious \"outcroppings\" on palette color region boundaries")
+    parser.add_argument("--dither", action="store_true", help="Use dithering to smooth palette color transitions")
+    parser.add_argument("--quantize", type=int, nargs=3, default=[8, 8, 8],
+                        help="Specify bit depth separately for R/G/B channels (1-8); "\
+                        "with very low values, the final palette may have fewer colors than desired")
+    parser.add_argument("--accent_percentile", type=float, default=98.0,
+                        help="Target \"badness\" percentile score for accent features. Setting this lower will "
+                        "use accent colors for a larger fraction of pixels. Range: (0,100)")
+    parser.add_argument("--resample", action="store_true",
+                        help="Resample color space after removing accent pixels")
+    parser.add_argument("--color_weights", type=float, nargs=3, default=[1.0, 1.0, 1.0],
+                        help="Skew the relative importance of the RGB channels")
+    parser.add_argument("--saturate", type=float, default=1.0,
+                        help="Change saturation of image by factor; saturating an image can improve quality")
+    parser.add_argument("--tint", type=float, nargs=2, default=[0.0, 0.0],
+                        help="Shift towards <hue [0, 360)> with <intensity [0, 1]>")
+
     args = parser.parse_args()
     return args
 
@@ -23,124 +48,172 @@ def save_file(fname, data, repeat):
     out = np.repeat(np.repeat(data, repeat, axis=0), repeat, axis=1)
     ski.io.imsave(fname, (out*256).astype(np.uint8))
 
+def saturate_and_tint(linear, saturation, tint_hue, tint_strength):
+    assert(saturation >= 0.0)
+    assert(0 <= tint_hue < 360)
+    assert(0 <= tint_strength <= 1)
+    for i in range(len(linear)):
+        hls = list(colorsys.rgb_to_hls(linear[i,0], linear[i,1], linear[i,2]))
+        if saturation <= 1.0:
+            hls[2] = saturation*hls[2]
+        else:
+            hls[2] = hls[2]**(1/saturation)
+        hls[0] = (1.0 - tint_strength) * hls[0] + tint_strength * (tint_hue / 360)
+        linear[i] = np.array(colorsys.hls_to_rgb(*hls))
+
+def make_palette(num_colors, data, extra_colors):
+    if num_colors > 0:
+        kmeans = MiniBatchKMeans(n_clusters=num_colors, random_state=0, n_init="auto", batch_size=1<<17)
+        kmeans = kmeans.fit(data)
+        palette = kmeans.cluster_centers_
+        if specific_colors is not None:
+            palette = np.concatenate((palette, extra_colors), axis=0)
+    else:
+        assert((extra_colors is not None) and (len(extra_colors) > 0))
+        palette = extra_colors
+    return palette
+
+@njit(parallel=True)
+def run_denoising(img_x, img_y, ori_edge, img_labels, palettized, final_palette, kernel_size, threshold):
+    assert(kernel_size%2 == 1)
+    kernel_pad = kernel_size // 2
+    done = False
+    iter = 0
+    while not done:
+        if iter == 10:
+            break
+        done = True
+        for x in prange(kernel_pad, img_x-kernel_pad):
+            for y in range(kernel_pad, img_y-kernel_pad):
+                if ori_edge[x, y]:
+                    continue
+                neighbor_labels = [img_labels[i, j] for i in range(x-kernel_pad,x+kernel_pad+1)
+                                                    for j in range(y-kernel_pad,y+kernel_pad+1)]
+                count = np.bincount(neighbor_labels)
+                if count.max() >= threshold:
+                    new_label = count.argmax()
+                    if new_label != img_labels[x, y]:
+                        img_labels[x, y] = new_label
+                        palettized[x, y] = final_palette[new_label]
+                        done = False
+        iter += 1
+
 def convert(in_fname, out_fname, block_size, palette_size, accent_size,
-            extra_colors, neighbor_threshold):
+            specific_colors, specific_accents, denoise, dither, quantize,
+            accent_percentile, resample, color_weights,
+            saturation, tint_hue, tint_strength):
+    quantize = np.array(quantize)
+    color_weights = np.array(color_weights)
+
+    assert(accent_size <= palette_size)
+    assert(quantize.min() >= 1)
+    assert(quantize.max() <= 8)
+    assert(0 < accent_percentile < 100)
+
     image = ski.io.imread(in_fname)
-    #target_res = (image.shape[0] // block_size, image.shape[1] // block_size)
+
+    if not all((image.shape[i] / block_size).is_integer() for i in range(2)):
+        print("Image size is not an integer multiple of block size; output image resolution will not match")
     img_x = image.shape[0] // block_size
     img_y = image.shape[1] // block_size
     downscaled = ski.transform.resize(image, (img_x, img_y), anti_aliasing=False)
 
     linear = downscaled.reshape(-1, 3)
+    if (saturation != 1.0) or (tint_strength != 0.0):
+        print("Adjust saturation and tint")
+        saturate_and_tint(linear, saturation, tint_hue, tint_strength)
 
-    #forest = IsolationForest(random_state=0, n_estimators=100, contamination=0.02).fit(linear)
-    #outlier = forest.predict(linear)
+    weighted = linear * color_weights
+    
+    print("Determining base palette")
+    base_palette_size = palette_size - accent_size
+    base_palette = make_palette(base_palette_size, weighted, specific_colors)
 
-    #luminance = np.array([1, 1, 1])
-    #luminance = np.array([0.2126, 0.7152, 0.0722])
+    if (accent_size > 0) or ((specific_accents is not None) and (len(specific_accents) > 0)):
+        print("Evaluating base palette badness")
+        base_color_deltas = base_palette.reshape(1, -1, 3) - weighted.reshape(-1, 1, 3)
+        base_delta_norm = (base_color_deltas**2).sum(axis=2)
+        base_labels = base_delta_norm.argmin(axis=1)
+        base_palettized_linear = base_palette[base_labels]
+        badness = (np.abs(base_palettized_linear-weighted)**2).sum(axis=1)
+        bad_threshold = np.percentile(badness, accent_percentile) * .95
+        bad_mask = badness >= bad_threshold
+        print(f"  Mean badness of {round(badness.mean(), 3)}; "\
+              f"{round(bad_mask.mean(), 3)} of pixels exceed threshold {round(bad_threshold, 3)}")
 
-    lum_color = linear * luminance
-    if palette_size > 0:
-        color_kmeans = KMeans(n_clusters=palette_size, random_state=0, n_init="auto").fit(lum_color)
-        labels = color_kmeans.predict(lum_color)
-        colors = color_kmeans.cluster_centers_[:,0:3]/luminance
-        colored = colors[labels]
-
-        badness = ((np.abs(linear-colored)*luminance)**2).sum(axis=1)
-        bad_threshold = np.percentile(badness, 98) * .95
-        if accent_size > 0:
-            accent_linear = linear[badness >= bad_threshold]
-            lum_accent = accent_linear * luminance
-            accent_kmeans = KMeans(n_clusters=accent_size, random_state=0, n_init="auto").fit(lum_accent)
-            labels = accent_kmeans.predict(lum_accent)
-            accents = accent_kmeans.cluster_centers_[:,0:3]/luminance
-        else:
-            accents = np.empty((0,3))
-        final_palette = np.concatenate((colors, extra_colors, accents), axis=0)
+        print("Determining accent palette")
+        accent_palette = make_palette(accent_size, weighted[bad_mask], specific_accents)
+        if resample:
+            base_palette = make_palette(base_palette_size, weighted[~bad_mask], specific_colors) # rerun w/o bad pixels
     else:
-        final_palette = extra_colors
+        bad_mask = np.zeros((len(weighted),)).astype(bool)
+        accent_palette = np.empty((0, 3))
 
-    lum_final_palette = final_palette * luminance
-    lum_color_deltas = lum_final_palette.reshape(1, -1, 3) - lum_color.reshape(-1, 1, 3)
-    lum_delta_norm = (lum_color_deltas**2).sum(axis=2)
-    if palette_size > 0:
-        lum_delta_norm[(badness<bad_threshold), palette_size+len(extra_colors):] = 1 << 30
-    final_colors = lum_delta_norm.argmin(axis=1)
-    img_labels = final_colors.reshape(img_x, img_y)
-    palettized = final_palette[final_colors].reshape(downscaled.shape)
+    weighted_palette = np.concatenate((base_palette, accent_palette) , axis=0)
+    print("Applying palette to image")
+    color_deltas = weighted_palette.reshape(1, -1, 3) - weighted.reshape(-1, 1, 3)
+    delta_norm = (color_deltas**2).sum(axis=2)
+    delta_norm[~bad_mask, len(base_palette):] = np.inf # Non-accent pixels can't have accent colors
+    labels = delta_norm.argmin(axis=1)
+    img_labels = labels.reshape(img_x, img_y)
+    final_palette = weighted_palette / color_weights
+    final_palette = np.clip(np.round(final_palette * ((1<<quantize)-1)) / ((1<<quantize)-1), 0.0, 0.9999)
+    palettized = final_palette[labels].reshape(downscaled.shape)
+    assert(palettized.min() >= 0)
+    assert(palettized.max() < 1)
 
-    #linear_palettized = colors[labels]
-    #palettized = linear_palettized.reshape(downscaled.shape)
+    if denoise or dither:
+        print("Run edge detection on original and final images")
+        ori_edge = ski.filters.scharr(downscaled).max(axis=2) > 0.1
+        pal_edge = ski.filters.scharr(palettized).max(axis=2) > 0.1
+        diff_edge = pal_edge & ~ori_edge # Spurious edges added by palettization
 
-    #diff = (np.abs(downscaled - palettized) * luminance).sum(axis=2)
+        if denoise:
+            print("Run denoising with kernel 3")
+            run_denoising(img_x, img_y, ori_edge, img_labels, palettized, final_palette, 3, 8)
+            print("Run denoising with kernel 5")
+            run_denoising(img_x, img_y, ori_edge, img_labels, palettized, final_palette, 5, 21)
 
-    ori_edge = ski.filters.scharr(downscaled).max(axis=2) > 0.1
-    pal_edge = ski.filters.scharr(palettized).max(axis=2) > 0.1
-    diff_edge = pal_edge & ~ori_edge
+        if dither:
+            print("Run dithering")
+            for x in range(img_x):
+                for y in range(img_y):
+                    if not diff_edge[x, y]:
+                        continue
+                    if (x > 0)\
+                       and (y%2 == 0)\
+                       and (img_labels[x-1, y] != img_labels[x, y])\
+                       and not ori_edge[x-1, y]:
+                        palettized[x-1, y] = final_palette[img_labels[x, y]]
+                        palettized[x, y] = final_palette[img_labels[x-1, y]]
+                    elif (y > 0)\
+                       and (x%2 == 0)\
+                       and (img_labels[x, y-1] != img_labels[x, y])\
+                       and not ori_edge[x, y-1]:
+                        palettized[x, y-1] = final_palette[img_labels[x, y]]
+                        palettized[x, y] = final_palette[img_labels[x, y-1]]
 
-    #palettized[diff_edge] = np.array([0.1, 0.9, 0.1])
-
-    # TODO: Eliminate outlying pixels ("outcroppings") first?
-    diff = -1
-    epoch = 0
-    while diff != 0:
-        if epoch == 10:
-            break
-        diff = 0
-        for x in range(1, img_x-1):
-            for y in range(1, img_y-1):
-                if ori_edge[x, y]:
-                    continue
-                neighbor_labels = [img_labels[i, j] for i in range(x-1,x+2) for j in range(y-1,y+2)]
-                count = np.bincount(neighbor_labels)
-                if count.max() >= neighbor_threshold:
-                    new_label = count.argmax()
-                    if new_label != img_labels[x, y]:
-                        img_labels[x, y] = new_label
-                        palettized[x, y] = final_palette[new_label]
-                        diff += 1
-        epoch += 1
-
-    for x in range(img_x):
-        for y in range(img_y):
-            if not diff_edge[x, y]:
-                continue
-            if (x > 0)\
-               and (y%2 == 0)\
-               and (img_labels[x-1, y] != img_labels[x, y])\
-               and not ori_edge[x-1, y]:
-                palettized[x-1, y] = final_palette[img_labels[x, y]]
-                palettized[x, y] = final_palette[img_labels[x-1, y]]
-            elif (y > 0)\
-               and (x%2 == 0)\
-               and (img_labels[x, y-1] != img_labels[x, y])\
-               and not ori_edge[x, y-1]:
-                palettized[x, y-1] = final_palette[img_labels[x, y]]
-                palettized[x, y] = final_palette[img_labels[x, y-1]]
-
-    #palettized[outlier.reshape(img_x, img_y)==-1] = np.array([0.1, 0.9, 0.1])
-    #palettized[diff > 4*np.median(diff)] = np.array([0.1, 0.9, 0.1])
-    #palettized[(badness >= bad_threshold).reshape(img_x, img_y)] = np.array([0.1, 0.9, 0.1])
-    #foo = (badness >= bad_threshold).reshape(img_x, img_y)
-    #palettized[foo] = downscaled[foo]
-
+    print("All done!")
     save_file(out_fname, palettized, block_size)
+
+def parse_colors(colors):
+    color_list = []
+    for c in colors:
+        assert(6 <= len(c) <= 7)
+        if len(c) == 7:
+            assert(c[0] == "#")
+            c = c[1:]
+        cr = int(c[0:2], 16) / 256
+        cg = int(c[2:4], 16) / 256
+        cb = int(c[4:6], 16) / 256
+        color_list.append([cr, cg, cb])
+    return np.array(color_list)
 
 if __name__ == "__main__":
     args = parse_arguments()
-    if args.specific_colors is not None:
-        extra_colors_list = []
-        for c in args.specific_colors:
-            assert(6 <= len(c) <= 7)
-            if len(c) == 7:
-                assert(c[0] == "#")
-                c = c[1:]
-            cr = int(c[0:2], 16) / 256
-            cg = int(c[2:4], 16) / 256
-            cb = int(c[4:6], 16) / 256
-            extra_colors_list.append([cr, cg, cb])
-        extra_colors = np.array(extra_colors_list)
-    else:
-        extra_colors = np.empty((0,3))
-    convert(args.input_filename, args.output_filename, args.block_size, args.palette_size, args.accent_palette_size,
-            extra_colors, args.neighbor_threshold)
+    specific_colors = parse_colors(args.specific_colors) if args.specific_colors is not None else None
+    specific_accents = parse_colors(args.specific_accents) if args.specific_accents is not None else None
+    convert(args.input_filename, args.output_filename, args.block_size, args.palette_size, args.accent_size,
+            specific_colors, specific_accents, args.denoise, args.dither, args.quantize,
+            args.accent_percentile, args.resample, args.color_weights,
+            args.saturate, *args.tint)
